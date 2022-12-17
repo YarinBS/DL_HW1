70b0e18de3c689a3ef78c89b8f8f956031fe8daa
YarinBS <yarin10121@gmail.com>
2022-12-17 14:13:29 +0200

    Update evaluation Q1's evaluation now returns the mode's KL Value Q2's evaluation now returns the model's accuracy on the MNIST test set

--- hw1_206230021_q2_eval.py
+++ hw1_206230021_q2_eval.py
@@ -1,44 +1,28 @@
-import winsound
 import pickle
 import torch
-
-from hw1_206230021_q2_train import model_1_train_and_test, model_2_train_and_test, model_3_train_and_test, \
-    model_4_train_and_test, model_5_train_and_test, fetch_MNIST_data
-
-# --- Sound constants ---
-DURATION = 1500  # milliseconds
-FREQ = 750  # Hz
+from hw1_206230021_q2_train import fetch_MNIST_data
 
 
 def evaluate():
-    model_1_train_and_test()
-    model_2_train_and_test()
-    model_3_train_and_test()
-    model_4_train_and_test()
-    model_5_train_and_test()
+    """
+    Print the accuracy of each model.
+    """
+    for i in range(5):
+        model = pickle.load(open(f'./models/q3/q3_model_{i + 1}.pkl', 'rb'))
+        _, _, test_data, test_loader = fetch_MNIST_data()
 
-    # -------
-    # Or:
+        accuracies = []
+        for (test_images, test_labels) in test_loader:
+            test_images = test_images.view(-1, 28 * 28)
+            test_outputs = model(test_images)
+            test_predictions = torch.argmax(test_outputs, dim=1)
+            accuracies.append(((test_predictions == test_labels).sum().item()) / test_labels.size(0))
 
-    # for i in range(5):
-    #     model = pickle.load(open(f'./models/q2/q2_model_{i+1}.pkl', 'rb'))
-    #     _, _, test_data, test_loader = fetch_MNIST_data()
-    #
-    #     accuracies = []
-    #     for (test_images, test_labels) in test_loader:
-    #         test_images = test_images.view(-1, 28 * 28)
-    #         test_outputs = model(test_images)
-    #         test_predictions = torch.argmax(test_outputs, dim=1)
-    #         accuracies.append(((test_predictions == test_labels).sum().item()) / test_labels.size(0))
-    #
-    #     print(f'Accuracy of model {i+1}: {100 * (sum(accuracies) / len(accuracies))}%')
+        print(f"Model {i + 1}'s accuracy: {round(100 * (sum(accuracies) / len(accuracies)), 3)}%")
 
 
 def main():
     evaluate()
-
-    # BEEP
-    winsound.Beep(FREQ, DURATION)
 
 
 if __name__ == '__main__':
